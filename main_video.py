0a349a9c5d316bc7190de660c4e945500b796734
ecspress <ecspress@hotmail.com>
2022-10-06 22:42:08 +0500

    fixed for forehead issue

--- main_video.py
+++ main_video.py
@@ -26,7 +26,7 @@ class VideoHandler(object):
             _, dst_img = self.video.read()
             dst_points, dst_shape, dst_face = select_face(dst_img, choose=False)
             if dst_points is not None:
-                dst_img = face_swap(self.src_face, dst_face, self.src_points, dst_points, dst_shape, dst_img, self.args, 68)
+                dst_img = face_swap(self.src_face, dst_face, self.src_points, dst_points, dst_shape, dst_img, self.args)
             self.writer.write(dst_img)
             # if self.args.show:
                 # cv2.imshow("Video", dst_img)
